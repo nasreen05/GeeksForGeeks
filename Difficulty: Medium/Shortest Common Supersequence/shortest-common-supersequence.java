fe54a6080aa3613354137bb57a5402ae5892f464
Reshma Nasreen <reshmanasreen30@gmail.com>
2025-11-11 11:29:20 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Shortest Common Supersequence/shortest-common-supersequence.java
@@ -0,0 +1,22 @@
+class Solution {
+    public static int minSuperSeq(String s1, String s2) {
+        int m = s1.length();
+        int n = s2.length();
+        int[][] dp = new int[m + 1][n + 1];
+
+        // Compute LCS
+        for (int i = 0; i <= m; i++) {
+            for (int j = 0; j <= n; j++) {
+                if (i == 0 || j == 0) {
+                    dp[i][j] = 0;
+                } else if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        // Length of SCS = sum of input lengths minus length of LCS
+        return m + n - dp[m][n];
+    }
+}
