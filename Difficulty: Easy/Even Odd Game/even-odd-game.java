f7efc4688acdc1dcf659da72572ff323c78e168d
Reshma Nasreen <reshmanasreen30@gmail.com>
2025-11-29 09:06:49 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Even Odd Game/even-odd-game.java
+++ Difficulty: Easy/Even Odd Game/even-odd-game.java
@@ -1,10 +1,19 @@
-class Solution {
-    public static String utility(int n) {
-        // If the number of apples is odd, "You" win; otherwise "Friend" wins
-        if (n % 2 == 1) {
-            return "You";
-        } else {
-            return "Friend";
+import java.util.*;
+
+public class Solution {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            
+            if (n % 2 == 1) {
+                System.out.println("You");
+            } else {
+                System.out.println("Friend");
+            }
         }
+        sc.close();
     }
 }
