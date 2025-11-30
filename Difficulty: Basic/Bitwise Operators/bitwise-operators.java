48b2f537439ed2d7f53ff19fbe82f7927926e949
Reshma Nasreen <reshmanasreen30@gmail.com>
2025-11-30 10:59:54 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Bitwise Operators/bitwise-operators.java
+++ Difficulty: Basic/Bitwise Operators/bitwise-operators.java
@@ -1,18 +1,17 @@
-import java.util.Scanner;
+class Solution {
+    public static void operators(int a, int b, int c) {
+         // code here
+        int d,e,f,g;
+        d = a ^ a;
+        e = c ^ b;
+        f = a & b;
+        g = c | (a ^ a);
+        e = ~ e;
+        
+        System.out.println(d +" "+ e +" "+ f +" "+ g);
 
-public class Solution {
-    public static void solve() {
-        Scanner scn = new Scanner(System.in);
-        int a = scn.nextInt();
-        int b = scn.nextInt();
-        int c = scn.nextInt();
-
-        int d = a ^ a;         // XOR of a with itself = 0
-        int e = c ^ b;         // XOR of c and b
-        e = ~e;                // NOT of e
-        int f = a & b;         // AND of a and b
-        int g = c | (a ^ a);   // OR of c with (a XOR a) which is c | 0 = c
-
-        System.out.println(d + " " + e + " " + f + " " + g);
     }
 }
+        
+    
+
