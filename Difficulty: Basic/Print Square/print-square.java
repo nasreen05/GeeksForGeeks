14bc7dda330634573ad93ea41bff6335ec59da44
Reshma Nasreen <reshmanasreen30@gmail.com>
2025-12-06 10:51:12 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Print Square/print-square.java
+++ Difficulty: Basic/Print Square/print-square.java
@@ -1,25 +1,19 @@
-class Solution {
-    public void square(int s) {
-        // Top border: print s asterisks with space between
-        for (int i = 0; i < s; i++) {
-            System.out.print("* ");
-        }
-        System.out.println();
+import java.util.*;
+
+public class Solution {
+    public static void solve() {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
         
-        // Middle rows: first * , spaces, last *
-        for (int i = 1; i < s - 1; i++) {
-            System.out.print("* ");
-            // Print (s-2) double spaces for hollow interior
-            for (int j = 1; j < s - 1; j++) {
-                System.out.print("  ");
-            }
-            System.out.println("*");
-        }
-        
-        // Bottom border (only if s > 1)
-        if (s > 1) {
-            for (int i = 0; i < s; i++) {
-                System.out.print("* ");
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                
+                // Print '*' for borders
+                if (i == 1 || i == n || j == 1 || j == n) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  ");
+                }
             }
             System.out.println();
         }
